d900948765a64a90b47c9958b76e72620732ddba
blueskyee <blueskyee9527@gmail.com>
2018-08-12 23:06:13 +0800

    update SymmetricTree_101.java

--- src/SymmetricTree_101.java
+++ src/SymmetricTree_101.java
@@ -1,6 +1,31 @@
+
 /**
  * Created by henry on 2018/8/9.
  */
 public class SymmetricTree_101 {
+    public static void main(String[] args){
+        try {
+            TreeNode ts = TreeNode.createSymmetricTree();
+            SymmetricTree_101 st = new SymmetricTree_101();
+            System.out.println(st.isSymmetric(ts));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        else return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if(left==null && right==null){
+            return true;
+        }else if(left==null || right==null || left.val!=right.val){
+            return false;
+        }else{
+            return isSymmetric(left.left, right.right) &&
+                    isSymmetric(left.right, right.left);
+        }
+    }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -32,4 +32,16 @@ public class TreeNode {
         return root;
     }
 
+    static public TreeNode createSymmetricTree() throws Exception{
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        return root;
+    }
+
 }
