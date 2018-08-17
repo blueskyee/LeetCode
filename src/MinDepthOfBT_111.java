240650b0aceb320bc71f8be95f4d9581743193d2
blueskyee <blueskyee9527@gmail.com>
2018-08-17 23:24:42 +0800

    update MinDepthOfBT_111.java

--- src/MinDepthOfBT_111.java
+++ src/MinDepthOfBT_111.java
@@ -15,17 +15,16 @@ public class MinDepthOfBT_111 {
     public int minDepth(TreeNode root) {
         if(root == null){
             return 0;
-        } else{
-            return leafDepth(root, 0);
+        } else if(root.left == null && root.right == null){
+            return 1;
+        }else if(root.left == null){
+            return minDepth(root.right)+1;
+        }else if(root.right == null){
+            return minDepth(root.left)+1;
+        }else{
+            return Math.min(minDepth(root.left),
+                    minDepth(root.right))+1;
         }
     }
 
-    public int leafDepth(TreeNode tn, int depth){
-        if (tn == null){
-            return depth;
-        }else{
-            return Math.min(leafDepth(tn.left, depth+1),
-                    leafDepth(tn.right, depth+1));
-        }
-    }
 }
