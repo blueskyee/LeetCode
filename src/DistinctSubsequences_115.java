7715f78549145af2514870fffb9a9678320c21a6
blueskyee <blueskyee9527@gmail.com>
2018-08-18 23:41:12 +0800

    add DistinctSubsequences_115.java

---
+++ src/DistinctSubsequences_115.java
@@ -0,0 +1,16 @@
+/**
+ * Created by henry on 2018/8/15.
+ */
+public class DistinctSubsequences_115 {
+    public static void main(String[] args){
+        String S = "babgbag";
+        String T = "bag";
+        DistinctSubsequences_115 ds = new DistinctSubsequences_115();
+        System.out.println(ds.numDistinct(S,T));
+    }
+
+
+    public int numDistinct(String s, String t) {
+        return 0;
+    }
+}
