91832635f3b2314937cc7891a25055898bb897a8
blueskyee <blueskyee9527@gmail.com>
2018-09-16 19:55:36 +0800

    add ValidAnagram_242.java

---
+++ src/ValidAnagram_242.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+/**
+ * Created by henry on 2018/9/16.
+ */
+public class ValidAnagram_242 {
+    public static void main(String[] args){
+        String s = "anagram";
+        String t = "nagaram";
+        ValidAnagram_242 va = new ValidAnagram_242();
+        System.out.println(va.isAnagram(s,t));
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
+    }
+}
