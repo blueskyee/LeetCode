d846a62cacfcc8b911634f071b91643d42149546
blueskyee <blueskyee9527@gmail.com>
2018-09-16 20:11:15 +0800

    add MinimumSizeSubarraySum_209.java

---
+++ src/MinimumSizeSubarraySum_209.java
@@ -0,0 +1,27 @@
+/**
+ * Created by henry on 2018/9/16.
+ */
+public class MinimumSizeSubarraySum_209 {
+    public static void main(String[] args){
+        int s = 7;
+        int[] nums = {2,3,1,2,4,3};
+        MinimumSizeSubarraySum_209 ms = new MinimumSizeSubarraySum_209();
+        System.out.println(ms.minSubArrayLen(s, nums));
+    }
+
+    public int minSubArrayLen(int s, int[] nums) {
+        int start = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        int numslength = nums.length;
+        for(int i=0; i<numslength; i++){
+            sum += nums[i];
+            while(sum >= s){
+                minLength = Math.min(minLength, i-start+1);
+                sum -= nums[start++];
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
