de9951c8828a58f8c7b0a842ca0ec8677d244945
blueskyee <blueskyee9527@gmail.com>
2018-10-05 18:41:28 +0800

    add MoveZeroes_283.java

---
+++ src/MoveZeroes_283.java
@@ -0,0 +1,21 @@
+/**
+ * Created by henry on 2018/10/5.
+ */
+public class MoveZeroes_283 {
+    public static void main(String[] args){
+        int[] nums = {0,1,0,3,12};
+        MoveZeroes_283 mz = new MoveZeroes_283();
+        System.out.println(nums);
+    }
+
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0)
+                nums[pos++] = nums[i];
+        }
+        for(int i=pos; i<nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
