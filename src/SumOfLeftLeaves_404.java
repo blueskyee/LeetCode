c8d27793ef0858bfd88a24eb69855abbe1b528b4
blueskyee <blueskyee9527@gmail.com>
2018-11-03 09:39:08 +0800

    update SymOfLeftLeaves_404.java

--- src/SumOfLeftLeaves_404.java
+++ src/SumOfLeftLeaves_404.java
@@ -16,7 +16,7 @@ public class SumOfLeftLeaves_404 {
 
     public int sumOfLeftLeaves(TreeNode root) {
         if(root==null) return 0;
-        
+
         int sum=0;
         if(root.left!=null){
             if(root.left.left==null && root.left.right==null){
