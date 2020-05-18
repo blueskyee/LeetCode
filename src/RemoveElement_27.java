c4ed54c2a942d28e3781d9c4a84906e37c79d5bd
blueskyee <blueskyee9527@gmail.com>
2020-05-18 15:15:52 +0800

    update

---
+++ src/RemoveElement_27.java
@@ -0,0 +1,32 @@
+/**
+ * Created by henry on 2018/7/25.
+ */
+public class RemoveElement_27 {
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        RemoveElement_27 re = new RemoveElement_27();
+        System.out.println(re.removeElement(nums, 2));
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int curIdx = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != val){
+                nums[curIdx] = nums[i];
+                curIdx++;
+            }
+        }
+        return curIdx;
+    }
+
+    /*public int removeElement(int[] nums, int val) {
+        int curIdx = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != val){
+                nums[curIdx] = nums[i];
+                curIdx++;
+            }
+        }
+        return curIdx;
+    }*/
+}
