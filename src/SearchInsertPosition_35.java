d897012c0c57c177013c6242d1e702b195eca534
blueskyee <blueskyee9527@gmail.com>
2020-05-20 09:33:18 +0800

    update

---
+++ src/SearchInsertPosition_35.java
@@ -0,0 +1,31 @@
+/**
+ * Created by henry on 2018/7/26.
+ */
+public class SearchInsertPosition_35 {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        SearchInsertPosition_35 sip = new SearchInsertPosition_35();
+        System.out.print(sip.searchInsert(nums, 0));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < target){
+                continue;
+            }else{
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    /*public int searchInsert(int[] nums, int target) {
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] >= target)
+                return i;
+            else
+                continue;
+        }
+        return nums.length;
+    }*/
+}
