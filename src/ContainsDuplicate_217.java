655c7b1182cd8bef7ca43d749ceafa13864596d5
blueskyee <blueskyee9527@gmail.com>
2020-05-30 20:48:38 +0800

    update

---
+++ src/ContainsDuplicate_217.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Created by henry on 2018/10/1.
+ */
+public class ContainsDuplicate_217 {
+    public static void main(String[] args){
+        int[] input = {1,2,3,1};
+        ContainsDuplicate_217 cd = new ContainsDuplicate_217();
+        System.out.println(cd.containsDuplicate(input));
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        //solution 1
+//        Arrays.sort(nums);
+//        for(int i=1; i<nums.length; i++){
+//            if(nums[i] == nums[i-1]) return true;
+//        }
+//        return false;
+
+        //solution2
+        HashSet<Integer> hm = new HashSet<Integer>();
+        for(int i: nums){
+            if(hm.contains(i))
+                return true;
+            hm.add(i);
+        }
+        return false;
+    }
+}
