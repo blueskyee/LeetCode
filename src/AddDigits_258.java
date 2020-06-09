058f88cf9febbdcc46e8b02cf2495e6db7083b17
blueskyee <blueskyee9527@gmail.com>
2020-06-09 10:16:04 +0800

    update

--- src/AddDigits_258.java
+++ src/AddDigits_258.java
@@ -3,12 +3,27 @@
  */
 public class AddDigits_258 {
     public static void main(String[] args){
+        int input = 1116;
         AddDigits_258 ad = new AddDigits_258();
-        System.out.println(ad.addDigits(112));
+        System.out.println(ad.addDigits(input));
+        System.out.println(ad.addDigitsLoop(input));
     }
 
     public int addDigits(int num) {
         if(num == 0) return 0;
-        return num == 9 ? 9 : num%9;
+        return (num - 1) % 9 + 1;
+    }
+
+    public int addDigitsLoop(int num) {
+        if(num < 10) return num;
+        while(num >= 10){
+            int tmp = 0;
+            while (num > 0){
+                tmp += num % 10;
+                num /= 10;
+            }
+            num = tmp;
+        }
+        return num;
     }
 }
