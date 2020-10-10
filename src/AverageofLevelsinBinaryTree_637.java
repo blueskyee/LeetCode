c81cd8b9b29eea11d07972f86a119fcf982c159e
blueskyee <blueskyee9527@gmail.com>
2020-10-10 20:33:14 +0800

    update

---
+++ src/AverageofLevelsinBinaryTree_637.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by henry on 2020/10/10.
+ */
+public class AverageofLevelsinBinaryTree_637 {
+    static public void main(String[] args) throws Exception {
+        TreeNode tn = TreeNode.createBST();
+        AverageofLevelsinBinaryTree_637 albt = new AverageofLevelsinBinaryTree_637();
+        List<Double> result = albt.averageOfLevels(tn);
+        for(Double d:result){
+            System.out.println(d);
+        }
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<Double>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        if(root == null) return result;
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int nodeNum = queue.size();
+            Double sum = 0.0;
+            for(int i=0; i<nodeNum; i++){
+                TreeNode t = queue.poll();
+                sum += t.val;
+                if(t.left != null) queue.offer(t.left);
+                if(t.right != null) queue.offer(t.right);
+            }
+            result.add(sum / nodeNum);
+        }
+
+        return result;
+    }
+}
+
