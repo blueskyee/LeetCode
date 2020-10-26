fbd5e3954bdac98c0febca1c88fd5e1ef502057c
blueskyee <blueskyee9527@gmail.com>
2020-10-26 14:10:45 +0800

    update

---
+++ src/IntersectionofTwoLinkedLists_160.java
@@ -0,0 +1,22 @@
+/**
+ * Created by henry on 2020/10/26.
+ */
+public class IntersectionofTwoLinkedLists_160 {
+    public static void main(String[] args){
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null)
+            return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+}
