d0815c14442fd7caefea1e55e3c3589a01e99fb9
blueskyee <blueskyee9527@gmail.com>
2020-11-04 22:44:59 +0800

    update

---
+++ src/PathSum_112.java
@@ -0,0 +1,19 @@
+/**
+ * Created by henry on 2020/11/4.
+ */
+public class PathSum_112 {
+    public static void main(String[] args) throws Exception {
+        TreeNode tn = TreeNode.createBST();
+        PathSum_112 ps = new PathSum_112();
+        System.out.println(ps.hasPathSum(tn, 22));
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(sum - root.val == 0 && root.left==null && root.right==null){
+            return true;
+        }else{
+            return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        }
+    }
+}
