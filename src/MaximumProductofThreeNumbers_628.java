f88ebfdf09ead7ca5c9a180ece3bb9def1ebdf6d
blueskyee <blueskyee9527@gmail.com>
2020-12-18 16:50:00 +0800

    update

--- src/MaximumProductofThreeNumbers_628.java
+++ src/MaximumProductofThreeNumbers_628.java
@@ -5,16 +5,13 @@ import java.util.Arrays;
  */
 public class MaximumProductofThreeNumbers_628 {
     public static void main(String[] args){
-        int[] input = {5,1,2,3,4};
+        int[] input = {1,2,3,0,-1};
         MaximumProductofThreeNumbers_628 mp = new MaximumProductofThreeNumbers_628();
         System.out.println(mp.maximumProduct(input));
     }
 
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
-        for(int i:nums){
-            System.out.println(i);
-        }
 
         return Math.max(nums[0]*nums[1]*nums[nums.length-1],
                 nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]);
