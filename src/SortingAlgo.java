5cbce24448860465f6b800cf6214b9a2cdfabcd5
blueskyee <blueskyee9527@gmail.com>
2021-01-07 00:03:20 +0800

    bubble sort

--- src/SortingAlgo.java
+++ src/SortingAlgo.java
@@ -198,4 +198,20 @@ public class SortingAlgo {
         }
     }
 
+
+    //bubble sort, simplest sorting algorithm
+    public static void bubbleSort(int[] number){
+        int n = number.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < (n - i); j++){
+                if(number[j - 1] > number[j]){
+                    //swap
+                    int temp = number[j];
+                    number[j] = number[j - 1];
+                    number[j - 1] = temp;
+                }
+            }
+        }
+    }
+
 }
