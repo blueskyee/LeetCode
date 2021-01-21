f0a1ce6522f6cc065048d8211fa52d05d62a7f99
blueskyee <blueskyee9527@gmail.com>
2021-01-21 23:21:12 +0800

    update

--- src/CombinationSumIV_377.java
+++ src/CombinationSumIV_377.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by henry on 2021/1/21.
  */
@@ -9,16 +11,45 @@ public class CombinationSumIV_377 {
         System.out.println(cs.combinationSum4(input, target));
     }
 
-    //recursive: Time Limit Exceeded
+    int[] dp;
     public int combinationSum4(int[] nums, int target) {
-        if(target == 0) return 1;
-        if(target < 0) return 0;
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        return calculate(nums, target);
+    }
+
+    private int calculate(int[] nums, int target){
+        //has been calculated before
+        if(dp[target] != -1) return dp[target];
 
         int res = 0;
         for(int i = 0; i < nums.length; i++){
-            res += combinationSum4(nums, target - nums[i]);
+            if(target >= nums[i]){
+                res += calculate(nums, target - nums[i]);
+            }
+        }
+        dp[target] = res;
+        return dp[target];
+    }
+
+    //recursive: Time Limit Exceeded
+    /*public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+        return calculate(nums, target);
+    }
+
+    private int calculate(int[] nums, int target){
+        if(target == 0) return 1;
+        if(target < 0) return -1;
+
+        int res = 0;
+        for(int i = 0; i < nums.length; i++){
+            int temp = combinationSum4(nums, target - nums[i]);
+            if(temp < 0) break;
+            res += temp;
         }
 
         return res;
-    }
+    }*/
 }
