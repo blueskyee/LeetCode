f73e307330e25694e09da3e0a9a73068e98e8aaf
blueskyee <blueskyee9527@gmail.com>
2021-01-22 15:52:26 +0800

    update

--- src/HouseRobber_198.java
+++ src/HouseRobber_198.java
@@ -9,17 +9,16 @@ public class HouseRobber_198 {
     }
 
     public int rob(int[] nums) {
-        int len = nums.length;
-        if(len == 0) return 0;
-        if(len == 1) return nums[0];
-        if(len == 2) return Math.max(nums[0], nums[1]);
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
 
-        nums[2] += nums[0];
-        for(int i=3; i<len; i++){
-            nums[i] += Math.max(nums[i-2], nums[i-3]);
+        int curInclude = nums[0], curExclude = 0;
+        for(int i = 1; i < nums.length; i++){
+            int preInclude = curInclude, preExclude = curExclude;
+            curInclude = preExclude + nums[i];
+            curExclude = Math.max(preInclude, preExclude);
         }
-
-        return Math.max(nums[len-1], nums[len-2]);
+        return Math.max(curInclude, curExclude);
     }
 
 /*    public int rob(int[] nums) {
