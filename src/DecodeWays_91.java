1becd657326a52f8d9808d912c95b97200107b40
blueskyee <blueskyee9527@gmail.com>
2021-01-25 00:15:19 +0800

    update

--- src/DecodeWays_91.java
+++ src/DecodeWays_91.java
@@ -5,8 +5,41 @@ import java.util.Set;
  * Created by henry on 2021/1/24.
  */
 public class DecodeWays_91 {
-    static Set<String> set = new HashSet<>();
 
+    public static void main(String[] args){
+        String str = "2201";
+        DecodeWays_91 dw = new DecodeWays_91();
+        System.out.println(dw.numDecodings(str));
+    }
+
+    public int numDecodings(String s) {
+        if(s == null || s.length() == 0) return 0;
+        Set<String> set = new HashSet<>();
+        for(int i = 1; i <= 26; i++){
+            set.add(String.valueOf(i));
+        }
+
+        int res = 0;
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        if(set.contains(s.substring(0, 1))){
+            dp[1] = 1;
+        }else{
+            return res;
+        }
+        for(int i = 2; i <= s.length(); i++){
+            if(set.contains(s.substring(i - 1, i))){
+                dp[i] += dp[i - 1];
+            }
+            if(set.contains(s.substring(i - 2, i))){
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[s.length()];
+    }
+
+    //Time Limit Exceeded
+    /*static Set<String> set = new HashSet<>();
     public static void main(String[] args){
         String str = "2101";
         DecodeWays_91 dw = new DecodeWays_91();
@@ -27,6 +60,5 @@ public class DecodeWays_91 {
             res += numDecodings(s.substring(2));
         }
         return res;
-    }
-
+    }*/
 }
