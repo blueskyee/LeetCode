34d985b4f671b4ddc3437615f68856ebfe5545a3
blueskyee <blueskyee9527@gmail.com>
2021-03-11 23:12:19 +0800

    update

---
+++ src/BinaryWatch_401.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by henry on 2021/3/11.
+ */
+public class BinaryWatch_401 {
+
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<>();
+        for(int h = 0; h < 12; h++){
+            for(int m = 0; m < 60; m++){
+                if(Integer.bitCount(h) + Integer.bitCount(m) == num){
+                    res.add(Integer.toString(h) + ":" + (m < 10 ? "0" + Integer.toString(m) : Integer.toString(m)));
+                }
+            }
+        }
+        return res;
+    }
+}
